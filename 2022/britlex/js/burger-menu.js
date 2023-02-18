22d2a33ebc538cbda9056c38dd8662b3e014cf55
OleksandrRyzhov <oleksandr.ryzhov@nure.ua>
2023-02-18 13:13:14 +0200

    Added focus & hide header

--- 2022/britlex/js/burger-menu.js
+++ 2022/britlex/js/burger-menu.js
@@ -6,30 +6,38 @@ const iconMenu = document.querySelector(".menu__icon");
 const menuBody = document.querySelector(".menu__body");
 if (iconMenu) {
   iconMenu.addEventListener("click", function (e) {
-    document.body.classList.toggle('_lock');
-    iconMenu.classList.toggle('_active');
-    menuBody.classList.toggle('_active');
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
   });
 }
 
 // scroll on click
 
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+const menuLinks = document.querySelectorAll(
+  ".menu__link[data-goto], .footer__logo[data-goto]"
+);
 if (menuLinks.length > 0) {
-  menuLinks.forEach(menuLink => {
+  menuLinks.forEach((menuLink) => {
     menuLink.addEventListener("click", onMenuLinkClick);
   });
 
   function onMenuLinkClick(e) {
     const menuLink = e.target;
-    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
       const gotoBlock = document.querySelector(menuLink.dataset.goto);
-      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector("header").offsetHeight;
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        scrollY -
+        document.querySelector("header").offsetHeight;
 
-      if (iconMenu.classList.contains('_active')) {
-        document.body.classList.remove('_lock');
-        iconMenu.classList.remove('_active');
-        menuBody.classList.remove('_active');
+      if (iconMenu.classList.contains("_active")) {
+        document.body.classList.remove("_lock");
+        iconMenu.classList.remove("_active");
+        menuBody.classList.remove("_active");
       }
 
       window.scrollTo({
@@ -40,3 +48,28 @@ if (menuLinks.length > 0) {
     }
   }
 }
+
+// Hide header
+let lastScroll = 0;
+const defaultOffset = 200;
+const header = document.querySelector(".header");
+
+const scrollPosition = () =>
+  window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains("hide");
+
+window.addEventListener("scroll", () => {
+  if (
+    scrollPosition() > lastScroll &&
+    !containHide() &&
+    scrollPosition() > defaultOffset
+  ) {
+    // scroll down
+    header.classList.add("hide");
+  } else if (scrollPosition() < lastScroll && containHide()) {
+    // scroll up
+    header.classList.remove("hide");
+  }
+
+  lastScroll = scrollPosition();
+});
