7f59bfb4f5f1f5f548525e41401ac89c78efc93b
OleksandrRyzhov <oleksandr.ryzhov@nure.ua>
2023-03-22 18:30:58 +0200

    added lazy loading image

---
+++ 2022/fanatic/js/lazy_load.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const lazyImages = document.querySelectorAll("img[data-src],source[data-srcset]");
+const windowHeight = document.documentElement.clientHeight;
+
+let lazyImagesPositions = [];
+
+if (lazyImages.length > 0) {
+  lazyImages.forEach((img) => {
+    if (img.dataset.src || img.dataset.srcset) {
+      lazyImagesPositions.push(img.getBoundingClientRect().top + scrollY);
+      lazyScrollCheck();
+    }
+  });
+}
+
+window.addEventListener("scroll", lazyScroll);
+
+function lazyScroll() {
+  if (document.querySelectorAll("img[data-src],source[data-srcset]").length > 0) {
+    lazyScrollCheck();
+  }
+}
+
+function lazyScrollCheck() {
+  let imgIndex = lazyImagesPositions.findIndex((item) => scrollY > item - windowHeight);
+  if (imgIndex >= 0) {
+    if (lazyImages[imgIndex].dataset.src) {
+      lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+      lazyImages[imgIndex].removeAttribute("data-src");
+    } else if (lazyImages[imgIndex].dataset.srcset) {
+      lazyImages[imgIndex].srcset = lazyImages[imgIndex].dataset.srcset;
+      lazyImages[imgIndex].removeAttribute("data-srcset");
+    }
+    delete lazyImagesPositions[imgIndex];
+  }
+}
+
+function ibg() {
+  let ibg = document.querySelectorAll(".ibg");
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector("img")) {
+      ibg[i].style.backgroundImage = "url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
+    }
+    if (ibg[i].querySelector("source")) {
+      ibg[i].style.backgroundImage = "url(" + ibg[i].querySelector("source").getAttribute("srcset") + ")";
+    }
+  }
+}
+
+ibg();
